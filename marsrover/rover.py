040da4cd9fef7c9b933ffd5747479b747d214716
Pablo Vieira <povieira@gmail.com>
2018-05-06 19:50:29 -0300

    (feat): add rotation to rover (turn left and right)

--- marsrover/rover.py
+++ marsrover/rover.py
@@ -7,12 +7,40 @@ class Rover(object):
     :arg initial_position: tuple with the initial position in the format: (x, y, orientation)
     :arg boundaries: plateau boundaries
     """
+
+    CARDINALS_ANGLES = {
+        'E': 0,
+        'N': 90,
+        'W': 180,
+        'S': 270,
+    }
+
+    # reverse index for getting cardinal position of a angle
+    ANGLES_CARDINALS = {angle: cardinal for cardinal, angle in CARDINALS_ANGLES.items()}
+
     def __init__(self, name, initial_position, boundaries):
         self.name = name
         self._x = initial_position[0]
         self._y = initial_position[1]
-        self._direction = initial_position[2]
+        self._direction_angle = self.CARDINALS_ANGLES.get(initial_position[2])
         self._boundaries = boundaries
 
+    @property
+    def current_position(self):
+        return f'{self.name}:{self._x} {self._y} {self.direction}'
+
+    @property
+    def direction(self):
+        return self.ANGLES_CARDINALS.get(self._direction_angle)
+
+    def _rotate(self, angle):
+        self._direction_angle = (self._direction_angle + angle) % 360
+
+    def turn_left(self):
+        self._rotate(90)
+
+    def turn_right(self):
+        self._rotate(-90)
+
     def __repr__(self):
-        return f'{self.name} ({self._x}, {self._y}, {self._direction})'
+        return f'{self.name} ({self._x}, {self._y}, {self.direction})'
--- tests/test_rover.py
+++ tests/test_rover.py
@@ -10,3 +10,32 @@ from marsrover.rover import Rover
 def test_rover_str():
     rover = Rover(name='RoverX', initial_position=(3, 5, 'N'), boundaries=(8, 13))
     assert str(rover) == 'RoverX (3, 5, N)'
+
+
+def test_rover_current_position():
+    rover = Rover(name='RoverX', initial_position=(3, 5, 'N'), boundaries=(8, 13))
+    assert rover.current_position == 'RoverX:3 5 N'
+
+
+def test_rover_turn_left():
+    rover = Rover(name='RoverX', initial_position=(3, 5, 'N'), boundaries=(8, 13))
+    rover.turn_left()
+    assert rover.current_position == 'RoverX:3 5 W'
+    rover.turn_left()
+    assert rover.current_position == 'RoverX:3 5 S'
+    rover.turn_left()
+    assert rover.current_position == 'RoverX:3 5 E'
+    rover.turn_left()
+    assert rover.current_position == 'RoverX:3 5 N'
+
+
+def test_rover_turn_right():
+    rover = Rover(name='RoverX', initial_position=(3, 5, 'N'), boundaries=(8, 13))
+    rover.turn_right()
+    assert rover.current_position == 'RoverX:3 5 E'
+    rover.turn_right()
+    assert rover.current_position == 'RoverX:3 5 S'
+    rover.turn_right()
+    assert rover.current_position == 'RoverX:3 5 W'
+    rover.turn_right()
+    assert rover.current_position == 'RoverX:3 5 N'
